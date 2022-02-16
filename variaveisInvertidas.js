e575a4a62c19bca560ad1c1d3c03b5a8dc6477f5
RafaelDuarteCoelho <96528447+RafaelDuarteCoelho@users.noreply.github.com>
2022-02-16 13:33:10 -0300

    Update variaveisInvertidas.js

--- variaveisInvertidas.js
+++ variaveisInvertidas.js
@@ -1,4 +1,4 @@
-let numeroX = [parseInt(prompt("Insira o número inteiro que será o valor de X"))];
+let numeroX = [parseInt(prompt("Insira o número inteiro que será o valor de X"))];      /* escolhi fazer em array e não com uma variavel auxiliar, pois queria manter um histórico das mudanças de valor das variáveis */
 validador = isNaN(numeroX[0]);
 while (validador){
     numeroX[0] = parseInt(prompt("não é um número, insira novamente"));
